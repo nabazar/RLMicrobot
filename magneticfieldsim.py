8ad481c82d512f6d68c93fd3ddaf15e842a579ac
Nematollah Ab Azar <47384926+nabazar@users.noreply.github.com>
2023-10-13 00:59:57 +0330

    Update magneticfieldsim.py

--- magneticfieldsim.py
+++ magneticfieldsim.py
@@ -23,8 +23,8 @@ class MagneticFieldSim():
     self.I=I
 
     ##Workspace Specifications
-    self.Lw=50e-3 #length,width and hight of the workspace
-    self.CoilPositions=np.array([[self.Lw,0,0],[0,self.Lw,0],[0,0,self.Lw],[-self.Lw,0,0],[0,-self.Lw,0],[0,0,-self.Lw]])#coils positions( x,y,z coordinates for 6x3: 6 coils)
+    self.Lw=2*self.Rc #length,width and hight of the workspace
+    self.CoilPositions=np.array([[self.Lw/2,0,0],[0,self.Lw/2,0],[0,0,self.Lw/2],[-self.Lw/2,0,0],[0,-self.Lw/2,0],[0,0,-self.Lw/2]])#coils positions( x,y,z coordinates for 6x3: 6 coils)
       
   def BiotSavar(self):
     beta=self.beta
