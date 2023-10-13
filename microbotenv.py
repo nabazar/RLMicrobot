226f1ea3c72efb40082fc296bb299298752ca405
Nematollah Ab Azar <47384926+nabazar@users.noreply.github.com>
2023-10-14 00:19:48 +0330

    Update microbotenv.py

--- microbotenv.py
+++ microbotenv.py
@@ -130,7 +130,7 @@ class Microrobot_Env():
     # theta_dot = 1.25*freq*M_z*(M_y*sin(phi_y)*sin(th-np.pi/2)+ M_x*sin(phi_x)*sin(th))
 
     I=[Ix,Iy,Iz,Ix2,Iy2,Iz2]
-    print(I)
+
     self.info=I
     theta=self.state
     P=np.array([x,y,0,1])
