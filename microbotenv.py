7ae3c447a7840d2bb87618e8929a48aff4b9fab9
Nematollah Ab Azar <47384926+nabazar@users.noreply.github.com>
2023-10-14 02:08:14 +0330

    Update microbotenv.py

--- microbotenv.py
+++ microbotenv.py
@@ -121,12 +121,12 @@ class Microrobot_Env():
     t=self.time
 
     mu1=1
-    Ix=mu1*action[0]
-    Iy=mu1*action[1]
-    Iz=mu1*action[2]
-    Ix2=-mu1*action[0]
-    Iy2=-mu1*action[1]
-    Iz2=-mu1*action[2]  
+    Ix=mu1*action[0][0]
+    Iy=mu1*action[0][1]
+    Iz=mu1*action[0][2]
+    Ix2=-mu1*action[0][0]
+    Iy2=-mu1*action[0][1]
+    Iz2=-mu1*action[0][2]  
     
     # theta_dot = 1.25*freq*M_z*(M_y*sin(phi_y)*sin(th-np.pi/2)+ M_x*sin(phi_x)*sin(th))
 
